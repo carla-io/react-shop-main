1a5f7bf4cfcc2cea7ad0697252d54d217d2360c0
Carla <dasalcarla812@gmail.com>
2024-10-05 23:11:18 +0800

    cartt

---
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './CartItem.css'
+
+const CartItems = () =>{
+    return (
+        <div className="cartItems">
+
+        </div>
+    )
+}
+
+export default CartItems;
