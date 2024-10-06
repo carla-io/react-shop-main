330c2d97b5c04217c16ec2a546ae480b452b67fe
Carla <dasalcarla812@gmail.com>
2024-10-06 19:49:59 +0800

    cart number and total

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
 
 const CartItems = () => {
-    const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+    const { getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
     return (
         <div className='cartItems'>
@@ -38,7 +38,7 @@ const CartItems = () => {
                         </div>
                     );
                 }
-                return null;  // Returning null if the condition is false to avoid the warning
+                return null;  
             })}
             <div className="cartItems-down">
                 <div className="cartItems-total">
@@ -46,7 +46,7 @@ const CartItems = () => {
                     <div>
                         <div className="cartItems-total-item">
                             <p>Subtotal</p>
-                            <p>${0}</p>
+                            <p>${getTotalCartAmount()}</p>
                         </div>
                         <hr />
                         <div className="cartItems-total-item">
@@ -56,7 +56,7 @@ const CartItems = () => {
                         <hr />
                         <div className="cartItems-total-item">
                             <h3>Total</h3>
-                            <h3>${0}</h3>
+                            <h3>${getTotalCartAmount()}</h3>
                         </div>
 
                     </div>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react'
 import all_product from '../Components/Assets/all_product'
+// import Item from '../Items/Item'
 
 export const ShopContext = createContext(null);
 
@@ -26,8 +27,30 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
      }
     
+     const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = all_product.find((product)=>product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+            
+        }
+        return totalAmount;
+     }
 
-     const contextValue = {all_product, cartItems, addToCart, removeFromCart};
+     const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                totalItem += cartItems[item];
+            }
+        }
+
+        return totalItem;
+     }
+
+     const contextValue = {getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
 
     return(
         <ShopContext.Provider value={contextValue}>
